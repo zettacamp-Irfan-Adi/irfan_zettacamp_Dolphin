332d55916efd331c1961098551727e69ba411149
zettacamp-Irfan-Adi <irfan.prananta@zettacamp.pro>
2023-08-25 16:04:22 +0700

    Commit task day 1 js variable

---
+++ task1.js
@@ -0,0 +1,38 @@
+// task1
+let book1 = "matahari";
+const book2 = "bulan";
+console.log(book1);
+console.log(book2);
+
+//1A
+book1="Light Novel";
+console.log(book1);
+
+//1B error karena cons tidak bisa dilakukan update
+// book2="bintang";
+// console.log(book2);
+
+//////////
+const buku2 = [];
+buku2.push('Naruto','solo leveling');
+console.log(buku2)
+const buku5 = [];
+buku5.push('Naruto','solo leveling');
+//////////
+const buk2 ={};
+buk2.judul = "I Obtained A Mythic Items";
+buk2.released =2022;
+console.log(buk2);
+// //////////
+
+//2
+const combination = book1+book2;
+console.log(combination);
+
+// const library = buku2.concat(buku5);
+// console.log(library);
+
+//undefined
+let a;
+// a=("satu");
+console.log(a);
