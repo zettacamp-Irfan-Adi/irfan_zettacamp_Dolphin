5a86adb46dc29f05d3f84c642e339d2fa4b5df03
zettacamp-Irfan-Adi <irfan.prananta@zettacamp.pro>
2023-08-25 15:03:26 +0700

    Commit task and maximum of two number

---
+++ task1.js
@@ -0,0 +1,45 @@
+//1A
+
+let book1="matahari";
+let book2="bulan";
+console.log(book1==book2);
+
+//2A
+let book1Price=25000;
+let book2Price=50000;
+if (book1Price > book2Price) {
+    console.log("book 1 more expensive than book 2");
+} else {
+    console.log("book 2 more expensive than book 1");
+}
+
+//2B
+let average = ((book1Price+book2Price)/2);
+console.log(average);
+
+//2C
+let MSI = 500000;
+let asus = 1660000;
+let average2 = ((MSI+asus)/2);
+console.log(average2<500000?"expensive":"cheap",average2);
+
+// console.log('\n',"*****************************");
+
+// let hargatotal=[];
+// let total=0;
+
+let totalbuku1=[{judul:"xxx",harga:1314},{judul:"xxx",harga:14525}];
+
+// for (let i = 0; i < totalbuku1.length; i++) {
+    
+    hargatotal.push(totalbuku1[i].harga);
+// }
+// for (let j = 0; j < hargatotal.length; j++){
+//     total +=hargatotal[j];
+// }
+// console.log(25%7);
+// console.log(10%3);
+// console.log(25/7);
+// console.log(10/3);
+
+// console.log(hargatotal)
